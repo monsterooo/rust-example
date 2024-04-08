954130d55cea8ce26b9170906da7d651f91f05f5
monsterooo <createthink.net@gmail.com>
2024-04-08 23:45:44 +0800

    增加tui.rs

--- ratatui-counter-async-app/src/main.rs
+++ ratatui-counter-async-app/src/main.rs
@@ -1,12 +1,39 @@
 use color_eyre::eyre::Result;
+use tokio::sync::mpsc::{self, UnboundedSender};
+use tui::Event;
+mod tui;
+
+// App 状态
+struct App {
+    counter: i64,
+    should_quit: bool,
+    action_tx: UnboundedSender<Action>,
+}
+
+// App 动作
+#[derive(Clone)]
+pub enum Action {
+    Tick,
+    Increment,
+    Decrement,
+    NetworkRequestAndThenIncrement,
+    NetworkRequestAndThenDecrement,
+    Quit,
+    Render,
+    None,
+}
+
+async fn run() -> Result<()> {
+    // 创建一个无界的通道用于通信
+    let (action_tx, mut action_rx) = mpsc::unbounded_channel();
+    let mut tui = tui::Tui::new()?.tick_rate(1.0).frame_rate(30.0);
+
+    Ok(())
+}
 
 #[tokio::main]
-async fn main() -> Result<()>{
-    startup()?;
-    
-    let result = run();
-
-    shutdown()?;
+async fn main() -> Result<()> {
+    let result = run().await;
 
     result?;
 
---
+++ ratatui-counter-async-app/src/tui.rs
@@ -0,0 +1,99 @@
+use std::{
+    ops::{Deref, DerefMut},
+    time::Duration,
+};
+
+use color_eyre::eyre::{Ok, Result};
+use crossterm::{
+    cursor,
+    event::{
+        DisableBracketedPaste, DisableMouseCapture, EnableBracketedPaste, EnableMouseCapture,
+        Event as CrosstermEvent, KeyEvent, KeyEventKind, MouseEvent,
+    },
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen},
+};
+use futures::{FutureExt, StreamExt};
+use ratatui::backend::CrosstermBackend as Backend;
+use serde::{Deserialize, Serialize};
+use tokio::{
+    sync::mpsc::{self, UnboundedReceiver, UnboundedSender},
+    task::JoinHandle,
+};
+use tokio_util::sync::CancellationToken;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum Event {
+    Init,
+    Quit,
+    Error,
+    Closed,
+    Tick,
+    Render,
+    FocusGained,
+    FocusLost,
+    Paste(String),
+    Key(KeyEvent),
+    Mouse(MouseEvent),
+    Resize(u16, u16),
+}
+
+pub struct Tui {
+    pub terminal: ratatui::Terminal<Backend<std::io::Stderr>>,
+    pub task: JoinHandle<()>,
+    pub cancellation_token: CancellationToken,
+    pub event_rx: UnboundedReceiver<Event>, // 接受事件通道
+    pub event_tx: UnboundedSender<Event>,   // 发送事件通道
+    pub frame_rate: f64,
+    pub tick_rate: f64,
+    pub mouse: bool,
+    pub paste: bool,
+}
+
+impl Tui {
+    pub fn new() -> Result<Self> {
+        let tick_rate = 4.0;
+        let frame_rate = 60.0;
+        let terminal = ratatui::Terminal::new(Backend::new(std::io::stderr()))?;
+        let (event_tx, event_rx) = mpsc::unbounded_channel();
+        let cancellation_token = CancellationToken::new();
+        let task = tokio::spawn(async {});
+        let mouse = false;
+        let paste = false;
+
+        Ok(Self {
+            terminal,
+            task,
+            cancellation_token,
+            event_rx,
+            event_tx,
+            frame_rate,
+            tick_rate,
+            mouse,
+            paste,
+        })
+    }
+
+    pub fn tick_rate(mut self, tick_rate: f64) -> Self {
+        self.tick_rate = tick_rate;
+        self
+    }
+
+    pub fn frame_rate(mut self, frame_rate: f64) -> Self {
+        self.frame_rate = frame_rate;
+        self
+    }
+
+    #[allow(dead_code)]
+    pub fn mouse(mut self, mouse: bool) -> Self {
+        self.mouse = mouse;
+        self
+    }
+
+    #[allow(dead_code)]
+    pub fn paste(mut self, paste: bool) -> Self {
+        self.paste = paste;
+        self
+    }
+
+    pub fn start() {}
+}
